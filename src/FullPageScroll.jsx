33377e2c670940c05381aa0de4e4e1ea3c5f7b45
Anir <anirpoke@gmail.com>
2023-02-12 00:06:05 +0100

    new components added

---
+++ src/FullPageScroll.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import  Fullpage , {FullPageSections,FullpageSection,FullpageNavigation}  from '@ap.cx/react-fullpage'
+
+function FullPageScroll() {
+  return (
+    <div>FullPageScroll</div>
+  )
+}
+
+export default FullPageScroll
